f4869241a1cb8e09488967b8275c0d5c368b9914
santiagoestrada <santiagoestrada.work@gmail.com>
2020-06-28 21:43:06 -0700

    Adding base pragma file needed to run PIC32MZ @ 84MHz

---
+++ PIC32/MZ/pragmas.h
@@ -0,0 +1,36 @@
+#ifndef PRAGMAS
+#define PRAGMAS "pragmas.h"
+
+// PIC32MZ2048ECG144 or EFG144 based HMZ144 board Configuration Bit Settings
+// DEVCFG2
+#if defined(__32MZ2048EFG144__)
+#pragma config FPLLIDIV = DIV_4         // System PLL Input Divider (4x Divider) for 24MHz clock (Rev D (C1) board) 24MHz/2 = 6MHz
+#pragma config UPLLFSEL = FREQ_24MHZ    // USB PLL Input Frequency Selection (USB PLL input is 24 MHz)
+#else
+#pragma config FPLLIDIV = DIV_2         // System PLL Input Divider (2x Divider) for 12 MHz crystal (Rev B and C boards) 12MHz/2 = 6MHz
+#pragma config UPLLEN = OFF             // USB PLL Enable (USB PLL is disabled)
+#endif
+#pragma config FPLLRNG = RANGE_5_10_MHZ // System PLL Input Range (5-10 MHz Input)
+#pragma config FPLLICLK = PLL_POSC      // System PLL Input Clock Selection (POSC is input to the System PLL)
+#pragma config FPLLMULT = MUL_112       // System PLL Multiplier (PLL Multiply by 112) 6MHz * 112 = 672MHz
+#pragma config FPLLODIV = DIV_8         // System PLL Output Clock Divider (8x Divider) 672MHz / 8 = 84MHz
+// DEVCFG1
+#pragma config FNOSC = SPLL             // Oscillator Selection Bits (Primary Osc (HS,EC))
+#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disable SOSC)
+#if defined(__32MZ2048EFG144__)
+#pragma config POSCMOD = EC             // Primary Oscillator Configuration EC - External clock osc
+#else
+#pragma config POSCMOD = HS             // Primary Oscillator Configuration HS - Crystal osc
+#endif
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disabled, FSCM Disabled)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled)
+#pragma config FDMTEN = OFF             // Deadman Timer Enable (Deadman Timer is disabled)
+#pragma config DMTINTV = WIN_127_128    // Default DMT Count Window Interval (Window/Interval value is 127/128 counter value)
+#pragma config DMTCNT = DMT31           // Max Deadman Timer count = 2^31
+// DEVCFG0
+#pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
+#pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (Communicate on PGEC2/PGED2)
+#include <xc.h>
+#include <sys/attribs.h>
+
+#endif
